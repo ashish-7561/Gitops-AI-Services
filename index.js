22883a0dc8c4dc1ce3da742b0f940c0017d21c15
ashish-7561 <ashishk56964@gmail.com>
2026-05-14 22:57:57 +0530

    fix: final configurations for k8s deployment

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    service: "Gitops-AI-Services",
+    status: "Online",
+    message: "AI Agentic Backend is running.",
+    timestamp: new Date().toISOString()
+  });
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('Healthy');
+});
+
+// Added '0.0.0.0' to allow the container to be reached by the K8s Service
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`AI Service running on port ${PORT}`);
+});
